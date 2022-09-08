e222ad8f37ae598dd8f11c442975b89882e428f8
Rohan Dhar <deusexmachina.dev@gmail.com>
2022-09-08 08:07:27 -0400

    feat: fixes

--- infra/index.ts
+++ infra/index.ts
@@ -5,7 +5,7 @@ import * as fs from 'fs'
 import * as pulumi from '@pulumi/pulumi'
 import { SharedInfraOutput, sharedOutputFileName } from './defs'
 import { createSharedInfra } from './shared'
-import { createStreamCluster, updateStreamEnvFile } from './stream'
+import { createStreamCluster, updateGqlEnvFile, updateStreamEnvFile } from './stream'
 
 export const sharedOutToJSONFile = (outMap: pulumi.automation.OutputMap): void => {
   const streamECRRepo = outMap.streamECRRepo.value
@@ -26,11 +26,18 @@ const main = async (): Promise<any> => {
   const args = process.argv.slice(2)
   const deployShared = args?.[0] === 'deploy:shared' || false
   const deployStream = args?.[0] === 'deploy:stream' || false
+  const buildGqlEnv = args?.[0] === 'gql:env' || false
   const buildStreamEnv = args?.[0] === 'stream:env' || false
 
   if (deployShared) {
     return createSharedInfra(true)
       .then(sharedOutToJSONFile)
+  }
+
+
+  if (buildGqlEnv) {
+    updateGqlEnvFile()
+    return
   }
 
   if (buildStreamEnv) {
--- infra/stream/index.ts
+++ infra/stream/index.ts
@@ -46,3 +46,25 @@ export const updateStreamEnvFile = (): void => {
   const targetFile = upath.joinSafe(env.workDir, '.env')
   fs.writeFileSync(targetFile, envfile.stringify(parsedFile))
 }
+
+export const updateGqlEnvFile = (): void => {
+  console.log('Read shared infra output from file...')
+  const infraOutput = getSharedInfraOutput()
+
+  console.log('Read stack yaml file...')
+  const ymlFileName = `Pulumi.${process.env.STAGE}.gql.${process.env.AWS_REGION}.yaml`
+  const ymlFile = upath.joinSafe(__dirname, 'stack', ymlFileName)
+  const ymlDoc = jyml.load(fs.readFileSync(ymlFile).toString()) as { [key: string]: any }
+  const stackConfig = ymlDoc.config as { [key: string]: string }
+
+  console.log('Update server environment file...')
+  const env = getEnv('gql', '.env.example')
+  let { parsedFile } = env
+  parsedFile = omit(parsedFile, 'PORT', 'DB_PORT', 'REDIS_PORT')
+  parsedFile['NODE_ENV'] = stackConfig['nftcom:nodeEnv']
+
+  console.log(JSON.stringify(parsedFile))
+
+  const targetFile = upath.joinSafe(env.workDir, '.env')
+  fs.writeFileSync(targetFile, envfile.stringify(parsedFile))
+}
