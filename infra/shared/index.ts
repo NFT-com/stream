21d2ca9181ef18a95479d3890c370f92e043fb48
Rohan Dhar <deusexmachina.dev@gmail.com>
2022-09-08 13:54:12 -0400

    fix: added env

--- infra/shared/index.ts
+++ infra/shared/index.ts
@@ -8,14 +8,20 @@ const pulumiProgram = async (): Promise<Record<string, any> | void> => {
   const config = new pulumi.Config()
   const stage = getStage()
   const sharedStack = new pulumi.StackReference(`${stage}.shared.us-east-1`);
+  const dbHost = sharedStack.getOutput('dbHost')
+  const redisHost = sharedStack.getOutput('redisHost')
   const vpc = sharedStack.getOutput('vpcId') 
   const subnets =  sharedStack.getOutput('publicSubnetIds') 
+  const dbHostVal: string = await pulumiOutToValue(dbHost)
+  const redisHostVal: string = await pulumiOutToValue(redisHost)
   const vpcVal: string = await pulumiOutToValue(vpc) 
   const subnetVal: string[] = await pulumiOutToValue(subnets)
 
   const sgs = createSecurityGroups(config, vpcVal) //hardcode test
   const { stream } = createRepositories()
   return {
+    dbHost: dbHostVal,
+    redisHost: redisHostVal,
     streamECRRepo: stream.name,
     publicSubnetIds: subnetVal,
     vpcId: vpcVal,
