fbf76b5c1fe31621d7685e67418081a5b11ae782
Jason Liu <jasonliudan@gmail.com>
2022-09-29 21:42:14 -0700

    feat: update updateNFTsForProfilesHandler

--- packages/stream/src/jobs/profile.handler.ts
+++ packages/stream/src/jobs/profile.handler.ts
@@ -65,50 +65,59 @@ export const updateNFTsForProfilesHandler = async (job: Job): Promise<any> => {
           if (!wallet) {
             logger.debug(`No wallet found for ID ${profile.ownerWalletId}`)
           } else {
-            // keep profile to cache, so we won't repeat profiles in progress
-            await cache.zadd(`${CacheKeys.PROFILES_IN_PROGRESS}_${chainId}`, 'INCR', 1, profile.id)
+            try {
+              // keep profile to cache, so we won't repeat profiles in progress
+              await cache.zadd(`${CacheKeys.PROFILES_IN_PROGRESS}_${chainId}`, 'INCR', 1, profile.id)
 
-            await checkNFTContractAddresses(profile.ownerUserId, wallet.id, wallet.address, chainId)
-            logger.debug(`checked NFT contract addresses for profile ${profile.id}`)
-            await updateWalletNFTs(profile.ownerUserId, wallet.id, wallet.address, chainId)
-            logger.debug(`updated wallet NFTs for profile ${profile.id}`)
-            await updateEdgesWeightForProfile(profile.id, profile.ownerWalletId)
-            logger.debug(`updated edges with weight for profile ${profile.id}`)
-            await syncEdgesWithNFTs(profile.id)
-            logger.debug(`synced edges with NFTs for profile ${profile.id}`)
-            // save visible NFT amount of profile
-            await saveVisibleNFTsForProfile(profile.id, repositories)
-            logger.debug(`saved amount of visible NFTs to profile ${profile.id}`)
-            // refresh NFTs for associated addresses
-            let msg = await updateNFTsForAssociatedAddresses(repositories, profile, chainId)
-            logger.debug(msg)
-            msg = await updateCollectionForAssociatedContract(
-              repositories,
-              profile,
-              chainId,
-              wallet.address,
-            )
-            logger.debug(msg)
-            // if gkIconVisible is true, we check if this profile owner still owns genesis key,
-            if (profile.gkIconVisible) {
-              await updateGKIconVisibleStatus(repositories, chainId, profile)
-              logger.debug(`gkIconVisible updated for profile ${profile.id}`)
-              const updateEnd = Date.now()
-              logger.debug(`updateNFTsForProfile for profile ${profile.id} took ${(updateEnd - updateBegin) / 1000} seconds`)
-            } else {
-              const updateEnd = Date.now()
-              logger.debug(`updateNFTsForProfile for profile ${profile.id} took ${(updateEnd - updateBegin) / 1000} seconds`)
+              await checkNFTContractAddresses(
+                profile.ownerUserId,
+                wallet.id,
+                wallet.address,
+                chainId,
+              )
+              logger.debug(`checked NFT contract addresses for profile ${profile.id}`)
+              await updateWalletNFTs(profile.ownerUserId, wallet.id, wallet.address, chainId)
+              logger.debug(`updated wallet NFTs for profile ${profile.id}`)
+              await updateEdgesWeightForProfile(profile.id, profile.ownerWalletId)
+              logger.debug(`updated edges with weight for profile ${profile.id}`)
+              await syncEdgesWithNFTs(profile.id)
+              logger.debug(`synced edges with NFTs for profile ${profile.id}`)
+              // save visible NFT amount of profile
+              await saveVisibleNFTsForProfile(profile.id, repositories)
+              logger.debug(`saved amount of visible NFTs to profile ${profile.id}`)
+              // refresh NFTs for associated addresses
+              let msg = await updateNFTsForAssociatedAddresses(repositories, profile, chainId)
+              logger.debug(msg)
+              msg = await updateCollectionForAssociatedContract(
+                repositories,
+                profile,
+                chainId,
+                wallet.address,
+              )
+              logger.debug(msg)
+              // if gkIconVisible is true, we check if this profile owner still owns genesis key,
+              if (profile.gkIconVisible) {
+                await updateGKIconVisibleStatus(repositories, chainId, profile)
+                logger.debug(`gkIconVisible updated for profile ${profile.id}`)
+                const updateEnd = Date.now()
+                logger.debug(`updateNFTsForProfile for profile ${profile.id} took ${(updateEnd - updateBegin) / 1000} seconds`)
+              } else {
+                const updateEnd = Date.now()
+                logger.debug(`updateNFTsForProfile for profile ${profile.id} took ${(updateEnd - updateBegin) / 1000} seconds`)
+              }
+              // save profile score
+              await saveProfileScore(repositories, profile)
+              logger.debug(`updated score for profile ${profile.id}`)
+              // 3. Once we update NFTs for profile, we cache it to UPDATED_NFTS_PROFILE with expire date
+              const now: Date = new Date()
+              now.setMilliseconds(now.getMilliseconds() + PROFILE_NFTS_EXPIRE_DURATION)
+              const ttl = now.getMilliseconds()
+              await cache.zadd(`${CacheKeys.UPDATED_NFTS_PROFILE}_${chainId}`, ttl, profile.id)
+              await cache.zrem(`${CacheKeys.UPDATE_NFTS_PROFILE}_${chainId}`, [profile.id])
+              await cache.zrem(`${CacheKeys.PROFILES_IN_PROGRESS}_${chainId}`, [profile.id])
+            } catch (err) {
+              logger.error(`Error in updateNFTsForProfilesHandler: ${err}`)
             }
-            // save profile score
-            await saveProfileScore(repositories, profile)
-            logger.debug(`updated score for profile ${profile.id}`)
-            // 3. Once we update NFTs for profile, we cache it to UPDATED_NFTS_PROFILE with expire date
-            const now: Date = new Date()
-            now.setMilliseconds(now.getMilliseconds() + PROFILE_NFTS_EXPIRE_DURATION)
-            const ttl = now.getMilliseconds()
-            await cache.zadd(`${CacheKeys.UPDATED_NFTS_PROFILE}_${chainId}`, ttl, profile.id)
-            await cache.zrem(`${CacheKeys.UPDATE_NFTS_PROFILE}_${chainId}`, [profile.id])
-            await cache.zrem(`${CacheKeys.PROFILES_IN_PROGRESS}_${chainId}`, [profile.id])
           }
         }
       }
