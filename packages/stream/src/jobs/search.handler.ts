0d238915be7c06f251cc8491989f998ed79d477c
t-morgan <tim@immutableholdings.com>
2023-01-03 14:49:14 -0500

    fix: need to await

--- packages/stream/src/jobs/search.handler.ts
+++ packages/stream/src/jobs/search.handler.ts
@@ -11,7 +11,7 @@ const repos = db.newRepositories()
 
 export const searchListingIndexHandler = async (job: Job): Promise<boolean> => {
   try {
-    const listings: entity.TxActivity[] = repos
+    const listings: entity.TxActivity[] = await repos
       .txActivity
       .findActivitiesNotExpired(defs.ActivityType.Listing, new Date(job.timestamp))
     if (listings) {
