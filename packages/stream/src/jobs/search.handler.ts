bc80597e9703754dbec14063333b75af61e07028
t-morgan <tim@immutableholdings.com>
2023-01-03 13:38:00 -0500

    feat: create search handler repeatable job

--- packages/stream/src/jobs/search.handler.ts
+++ packages/stream/src/jobs/search.handler.ts
@@ -3,16 +3,20 @@ import { Job } from 'bull'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { searchEngineService } from '@nftcom/gql/service'
-import { _logger,entity, utils } from '@nftcom/shared'
+import { _logger, db, defs, entity, utils } from '@nftcom/shared'
 
 const logger = _logger.Factory('search.handler', _logger.Context.Bull)
 const seService = searchEngineService.SearchEngineService()
+const repos = db.newRepositories()
 
 export const searchListingIndexHandler = async (job: Job): Promise<boolean> => {
   try {
-    const { listings }: { listings: entity.TxOrder[] } = job.data
+    const { listings }: { listings: entity.TxOrder[] } = repos
+      .txActivity
+      .findActivitiesNotExpired(defs.ActivityType.Listing, new Date(job.timestamp))
+      .map((txActivity) => txActivity.order)
     if (listings) {
-      const nftsWithListingUpdates = await utils.getNFTsFromTxOrders(listings)
+      const nftsWithListingUpdates = await utils.getNFTsFromTxActivities(listings)
       seService.indexNFTs(nftsWithListingUpdates)
     }
     return Promise.resolve(true)
