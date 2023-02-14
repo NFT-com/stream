14af288f4eec5e9649735bb8b1f67fdd175b3c44
Rohan Dhar <deusexmachina.dev@gmail.com>
2023-02-14 10:13:46 -0500

    feat: ownership update

--- packages/stream/src/service/ownership.ts
+++ packages/stream/src/service/ownership.ts
@@ -7,6 +7,7 @@ import { _logger, contracts, db, defs, entity, helper } from '@nftcom/shared'
 
 import { cache, CacheKeys } from './cache'
 
+const MAX_PROCESS_BATCH_SIZE = 100
 const logger = _logger.Factory(_logger.Context.NFT)
 const repositories = db.newRepositories()
 
@@ -57,26 +58,57 @@ export const updateOwnership = async (
           profileId: null,
         })
       } else {
-        const cachePromise = []
+        let cachePromise = []
         // if this NFT is existing and owner changed, we change its ownership...
         if (existingNFT.userId !== wallet.userId || existingNFT.walletId !== wallet.id) {
           // we remove edge of previous profile
           // logger.log(`&&& updateNFTOwnershipAndMetadata: existingNFT.userId ${existingNFT.userId}, userId ${userId}, existingNFT.walletId ${existingNFT.walletId}, walletId ${walletId}`)
-          const edges: entity.Edge[] =  await repositories.edge.find({
-            where: {
-              thatEntityId: existingNFT.id,
-              edgeType: defs.EdgeType.Displays,
-            },
-          })
-        
-          for (const edge of edges) {
-            const key1 = `${CacheKeys.PROFILE_SORTED_VISIBLE_NFTS}_${chainId}_${edge.thisEntityId}`,
-              key2 = `${CacheKeys.PROFILE_SORTED_NFTS}_${chainId}_${edge.thisEntityId}`
-            cachePromise.push(
-              cache.keys(`${key1}*`),
-              cache.keys(`${key2}*`),
-            )
-            logger.log(`old profileId: ${edge.thisEntityId}, key1: ${key1}, key2: ${key2}.`)
+         
+          const oldOwnerProfileQuery = {
+            ownerWalletId: existingNFT.walletId,
+            ownerUserId: existingNFT.userId,
+          }
+         
+          const oldOwnerProfileCount: number = await repositories.profile
+            .count(oldOwnerProfileQuery)
+
+          for (let i=0; i < oldOwnerProfileCount; i+= MAX_PROCESS_BATCH_SIZE) {
+            const oldOwnerProfiles: entity.Profile[] = await repositories.profile.find({
+              where:  oldOwnerProfileQuery,
+              skip: i,
+              take: MAX_PROCESS_BATCH_SIZE,
+              select: {
+                id: true,
+                url: true,
+              },
+            })
+          
+            for (const profile of oldOwnerProfiles) {
+              const key1 = `${CacheKeys.PROFILE_SORTED_VISIBLE_NFTS}_${chainId}_${profile.id}`,
+                key2 = `${CacheKeys.PROFILE_SORTED_NFTS}_${chainId}_${profile.id}`
+              cachePromise.push(
+                cache.keys(`${key1}*`),
+                cache.keys(`${key2}*`),
+              )
+              logger.log(`old profileId: ${profile.id}, key1: ${key1}, key2: ${key2}.`)
+            }
+
+            if (cachePromise.length) {
+              try {
+                const keysArray = await Promise.all(cachePromise)
+                if (keysArray.length) {
+                  for (const keys of keysArray) {
+                    if (keys?.length) {
+                      await cache.del(...keys)
+                      logger.log(`Key deleted: ${keys}`)
+                    }
+                  }
+                }
+                cachePromise = []
+              } catch (err) {
+                logger.log(err, 'Error while clearing cache...')
+              }
+            }
           }
 
           await repositories.edge.hardDelete({
@@ -105,74 +137,71 @@ export const updateOwnership = async (
               logger.info(`previous wallet for existing NFT ${existingNFT.id} is undefined`)
             }
           }
+       
           updatedNFT = await repositories.nft.updateOneById(existingNFT.id, {
             owner: csNewOwner,
             userId: wallet.userId,
             walletId: wallet.id,
           })
-
-          // new owner profile
-          const newOwnerProfiles: entity.Profile[] = await repositories.profile.find({ where: {
-            ownerWalletId: wallet.id,
-            ownerUserId: wallet.userId,
-          } })
-
-          for (const profile of newOwnerProfiles) {
-            // 
-            try {
-              await repositories.edge.save({
-                thisEntityType: defs.EntityType.Profile,
-                thatEntityType: defs.EntityType.NFT,
-                thisEntityId: profile.id,
-                thatEntityId: updatedNFT.id,
-                edgeType: defs.EdgeType.Displays,
-                hide: true,
-              })
-              logger.info(`updated edges for profile in ownership for profileId: ${profile.id}, url: ${profile.url}.`)
-            } catch (err) {
-              logger.error(err, `Error in updateEdgesWeightForProfile in ownership for profileId:${profile.id}, url: ${profile.url}`)
-            }
-
-            try {
-              await nftService.syncEdgesWithNFTs(profile.id)
-              logger.info(`synced edges with NFTs for profile in ownership for profileId: ${profile.id}, url: ${profile.url}`)
-            } catch (err) {
-              logger.error(err, `Error in syncEdgesWithNFTs in ownership for profileId:${profile.id}, url: ${profile.url}`)
-            }
-             
-            try {
-              await Promise.all([
-                cache.zrem(`${CacheKeys.PROFILES_IN_PROGRESS}_${chainId}`, [profile.id]),
-                cache.zrem(`${CacheKeys.UPDATED_NFTS_PROFILE}_${chainId}`, [profile.id]),
-              ])
-              await nftService.executeUpdateNFTsForProfile(profile.id, chainId)
-              logger.info(`executeUpdateNFTsForProfile in ownership for profileId: ${profile.id}, url: ${profile.url}`)
-            } catch (err) {
-              logger.error(err, `Error in executeUpdateNFTsForProfile in ownership for profileId:${profile.id}, url: ${profile.url}`)
-            }
+          if(updatedNFT) {
+            await nftService.indexNFTsOnSearchEngine([updatedNFT])
           }
 
-          if (cachePromise.length) {
-            try {
-              const keysArray = await Promise.all(cachePromise)
-              if (keysArray.length) {
-                for (const keys of keysArray) {
-                  if (keys?.length) {
-                    await cache.del(...keys)
-                    logger.log(`Key deleted: ${keys}`)
-                  }
-                }
+          // new owner profile
+          const profileQuery = {
+            ownerWalletId: wallet.id,
+            ownerUserId: wallet.userId,
+          }
+          const newOwnerProfileCount: number = await repositories.profile.count(profileQuery)
+
+          for (let i=0; i < newOwnerProfileCount; i += MAX_PROCESS_BATCH_SIZE) {
+            const newOwnerProfiles: entity.Profile[] = await repositories.profile.find({
+              where:  profileQuery,
+              skip: i,
+              take: MAX_PROCESS_BATCH_SIZE,
+              select: {
+                id: true,
+                url: true,
+              },
+            })
+  
+            for (const profile of newOwnerProfiles) {
+              // 
+              try {
+                await repositories.edge.save({
+                  thisEntityType: defs.EntityType.Profile,
+                  thatEntityType: defs.EntityType.NFT,
+                  thisEntityId: profile.id,
+                  thatEntityId: updatedNFT.id,
+                  edgeType: defs.EdgeType.Displays,
+                  hide: true,
+                })
+                logger.info(`updated edges for profile in ownership for profileId: ${profile.id}, url: ${profile.url}.`)
+              } catch (err) {
+                logger.error(err, `Error in updateEdgesWeightForProfile in ownership for profileId:${profile.id}, url: ${profile.url}`)
               }
-            } catch (err) {
-              logger.log(err, 'Error while clearing cache...')
+  
+              try {
+                await nftService.syncEdgesWithNFTs(profile.id)
+                logger.info(`synced edges with NFTs for profile in ownership for profileId: ${profile.id}, url: ${profile.url}`)
+              } catch (err) {
+                logger.error(err, `Error in syncEdgesWithNFTs in ownership for profileId:${profile.id}, url: ${profile.url}`)
+              }
+               
+              try {
+                await Promise.all([
+                  cache.zrem(`${CacheKeys.PROFILES_IN_PROGRESS}_${chainId}`, [profile.id]),
+                  cache.zrem(`${CacheKeys.UPDATED_NFTS_PROFILE}_${chainId}`, [profile.id]),
+                ])
+                //await nftService.executeUpdateNFTsForProfile(profile.id, chainId)
+                // logger.info(`executeUpdateNFTsForProfile in ownership for profileId: ${profile.id}, url: ${profile.url}`)
+              } catch (err) {
+                logger.error(err, `Error in executeUpdateNFTsForProfile in ownership for profileId:${profile.id}, url: ${profile.url}`)
+              }
             }
           }
         }
       }
-    }
-
-    if(updatedNFT) {
-      await nftService.indexNFTsOnSearchEngine([updatedNFT])
     }
   }
 }
