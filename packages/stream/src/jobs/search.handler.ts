431e60f4a1042861d05f59b47aac07554c6a826c
Rohan Dhar <deusexmachina.dev@gmail.com>
2023-02-17 09:11:26 -0500

    fix: let search engine know when lisitng is expired

--- packages/stream/src/jobs/search.handler.ts
+++ packages/stream/src/jobs/search.handler.ts
@@ -20,7 +20,7 @@ export const searchListingIndexHandler = async (job: Job): Promise<boolean> => {
         updatedAt: MoreThanOrEqual(new Date(job.timestamp)),
       }, {
         activityType: defs.ActivityType.Listing,
-        updatedAt: LessThanOrEqual(Date.now()),
+        expiration: LessThanOrEqual(Date.now()),
       }],
       })
     logger.log(`total listings in search index sync: ${listings.length}`)
