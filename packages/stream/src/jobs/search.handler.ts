7aa787efe13d718ec3c23b98ee82388553285615
Rohan Dhar <deusexmachina.dev@gmail.com>
2023-02-21 09:23:52 -0500

    feat: add expiered listings

--- packages/stream/src/jobs/search.handler.ts
+++ packages/stream/src/jobs/search.handler.ts
@@ -1,5 +1,5 @@
 import { Job } from 'bull'
-import { MoreThanOrEqual } from 'typeorm'
+import { Between, MoreThanOrEqual } from 'typeorm'
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -15,10 +15,19 @@ export const searchListingIndexHandler = async (job: Job): Promise<boolean> => {
   try {
     const listings: entity.TxActivity[] = await repos
       .txActivity
-      .find({ where: {
-        activityType: defs.ActivityType.Listing,
-        updatedAt: MoreThanOrEqual(new Date(job.timestamp)),
-      },
+      .find({ where: [
+        {
+          activityType: defs.ActivityType.Listing,
+          updatedAt: MoreThanOrEqual(new Date(job.timestamp)),
+        },
+        {
+          activityType: defs.ActivityType.Listing,
+          expiration: Between(
+            new Date(job.timestamp),
+            new Date(),
+          ),
+        },
+      ],
       })
     logger.log(`total listings in search index sync: ${listings.length}`)
     if (listings) {
