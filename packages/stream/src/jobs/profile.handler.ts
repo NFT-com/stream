25555aa308db19418ebcfd9ccd4b291f4fccd975
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-03-15 13:13:27 -0400

    fix: bump

--- packages/stream/src/jobs/profile.handler.ts
+++ packages/stream/src/jobs/profile.handler.ts
@@ -17,8 +17,8 @@ const repositories = db.newRepositories()
 
 const PROFILE_NFTS_EXPIRE_DURATION = Number(process.env.PROFILE_NFTS_EXPIRE_DURATION)
 const PROFILE_PROGRESS_THRESHOLD = Number(process.env.PROFILE_PROGRESS_THRESHOLD || 10)
-const MAX_NFTS_TO_PROCESS = 100000 // max nfts to process per batch
-const MAX_NFTS_PER_PROFILE = 50000 // max nfts to process per profile
+const MAX_NFTS_TO_PROCESS = 100000 // max nfts to process / batch
+const MAX_NFTS_PER_PROFILE = 50000 // max nfts to process / profile
 
 export const nftUpdateBatchProcessor = async (job: Job): Promise<boolean> => {
   logger.info(`initiated nft update batch processor for profile ${job.data.profileId} - index : ${job.data.index}`)
