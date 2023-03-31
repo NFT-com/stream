02bc021728055f85150325f359f368552d29b96e
gmaijoe <gmaijoe@users.noreply.github.com>
2023-03-31 14:21:40 -0400

    Setup PG from streaming fast (#256)
    * fix: initial commit
    
    * fix: add to streams
    
    * fix: update
    
    * fix: add filters
    
    * Feat/streamingfast nft exists (#257)
    
    * fix: filter out non repeatable queues from creating initial jobs (#255)
    
    * fix: filter out non repeatable queues from creating initial jobs (#255)
    
    * feat: naieve only existing nft filter
    
    * fix: get code in a runnable state
    
    * fix: await handleFilter result and use BigNumber to get hexString
    
    * fix: better logs
    
    * chore: remove debug loggers
    
    * fix: added timestamps
    
    * fix: add tokenIOd
    
    * fix: ok
    
    * fix: update hex
    
    * fix: tokenId
    
    * fix: fix
    
    * fix: increase time to 10 minutes for update wallets
    
    * fix: remove official filter
    
    * fix: refactor
    
    * fix: bugs
    
    * fix: more logs
    
    * fix: logs
    
    * fix: update
    
    ---------
    
    Co-authored-by: Tim Morgan <2998891+t-morgan@users.noreply.github.com>
    Co-authored-by: t-morgan <tim@immutableholdings.com>

--- packages/stream/src/service/ownership.ts
+++ packages/stream/src/service/ownership.ts
@@ -19,6 +19,245 @@ export const checksumAddress = (address: string): string | undefined => {
   }
   return
 }
+
+/**
+ * Processes the Profile NFT.
+ * @param existingNFT - The existing NFT entity in the database.
+ * @param chainId - The chain ID of the blockchain network.
+ */
+const processProfileNFT = async (existingNFT: entity.NFT): Promise<void> => {
+  logger.info(`Profile is being processed as a Profile NFT - contract ${existingNFT.contract}, tokenId: ${existingNFT.tokenId}`)
+  const previousWallet = await repositories.wallet.findById(existingNFT.walletId)
+  if (previousWallet) {
+    const profile: entity.Profile = await repositories.profile.findOne({ where: {
+      tokenId: BigNumber.from(existingNFT.tokenId).toString(),
+      ownerWalletId: previousWallet.id,
+      ownerUserId: previousWallet.userId,
+    } })
+
+    // If this NFT was the previous owner's preferred profile...
+    if (profile && profile?.id === previousWallet.profileId) {
+      await repositories.wallet.updateOneById(previousWallet.id, {
+        profileId: null,
+      })
+    }
+  } else {
+    logger.info(`previous wallet for existing NFT ${existingNFT.id} is undefined`)
+  }
+}
+
+/**
+ * Handles the new owner profile.
+ * @param wallet - The wallet of the new owner.
+ * @param updatedNFT - The updated NFT entity.
+ * @param chainId - The chain ID of the blockchain network.
+ * @returns A Promise that resolves to void.
+ */
+const handleNewOwnerProfile = async (
+  wallet: entity.Wallet,
+  updatedNFT: entity.NFT,
+  chainId: string,
+): Promise<void> => {
+  const profileQuery = {
+    ownerWalletId: wallet.id,
+    ownerUserId: wallet.userId,
+  }
+  const newOwnerProfileCount: number = await repositories.profile.count(profileQuery)
+
+  logger.info(`New owner profiles count: ${newOwnerProfileCount}`)
+
+  for (let i = 0; i < newOwnerProfileCount; i += MAX_PROCESS_BATCH_SIZE) {
+    const newOwnerProfiles: entity.Profile[] = await repositories.profile.find({
+      where: profileQuery,
+      skip: i,
+      take: MAX_PROCESS_BATCH_SIZE,
+      select: {
+        id: true,
+        url: true,
+      },
+    })
+
+    logger.info(`New owner profiles length: ${newOwnerProfiles.length}, batch: ${i}`)
+
+    for (const profile of newOwnerProfiles) {
+      try {
+        await repositories.edge.save({
+          thisEntityType: defs.EntityType.Profile,
+          thatEntityType: defs.EntityType.NFT,
+          thisEntityId: profile.id,
+          thatEntityId: updatedNFT.id,
+          edgeType: defs.EdgeType.Displays,
+          hide: true,
+        })
+        logger.info(`updated edges for profile in ownership for profileId: ${profile.id}, url: ${profile.url}.`)
+      } catch (err) {
+        logger.error(err, `Error in updateEdgesWeightForProfile in ownership for profileId:${profile.id}, url: ${profile.url}`)
+      }
+
+      try {
+        await nftService.syncEdgesWithNFTs(profile.id)
+        logger.info(`synced edges with NFTs for profile in ownership for profileId: ${profile.id}, url: ${profile.url}`)
+      } catch (err) {
+        logger.error(err, `Error in syncEdgesWithNFTs in ownership for profileId:${profile.id}, url: ${profile.url}`)
+      }
+
+      try {
+        await Promise.all([
+          cache.zrem(`${CacheKeys.PROFILES_IN_PROGRESS}_${chainId}`, [profile.url]),
+          cache.zrem(`${CacheKeys.UPDATED_NFTS_PROFILE}_${chainId}`, [profile.url]),
+        ])
+      } catch (err) {
+        logger.error(err, `Error in removing cache in ownership for profileId:${profile.id}, url: ${profile.url}`)
+      }
+    }
+  }
+}
+
+// Add these utility functions at the top of the file or in a separate utility file
+const removePreviousProfileEdge = async (nftId: string): Promise<void> => {
+  try {
+    await repositories.edge.hardDelete({
+      thatEntityId: nftId,
+      edgeType: defs.EdgeType.Displays,
+    })
+  } catch (err) {
+    logger.error(err, `Error in removing previous profile edge for walletId: nftId: ${nftId}`)
+  }
+}
+
+const deleteCacheKeys = async (existingNFT: entity.NFT, chainId: string): Promise<void> => {
+  try {
+    const oldOwnerProfileQuery = {
+      ownerWalletId: existingNFT.walletId,
+      ownerUserId: existingNFT.userId,
+    }
+   
+    const oldOwnerProfileCount: number = await repositories.profile
+      .count(oldOwnerProfileQuery)
+
+    logger.log(`Old owner profiles count: ${oldOwnerProfileCount} - query: ${JSON.stringify(oldOwnerProfileQuery)}`)
+
+    for (let i=0; i < oldOwnerProfileCount; i+= MAX_PROCESS_BATCH_SIZE) {
+      const oldOwnerProfiles: entity.Profile[] = await repositories.profile.find({
+        where:  oldOwnerProfileQuery,
+        skip: i,
+        take: MAX_PROCESS_BATCH_SIZE,
+        select: {
+          id: true,
+          url: true,
+        },
+      })
+    
+      logger.info(`Old owner profiles length: ${oldOwnerProfiles.length}, batch: ${i}`)
+      let cachePromise = []
+      for (const profile of oldOwnerProfiles) {
+        const key1 = `${CacheKeys.PROFILE_SORTED_VISIBLE_NFTS}_${chainId}_${profile.id}`,
+          key2 = `${CacheKeys.PROFILE_SORTED_NFTS}_${chainId}_${profile.id}`
+        cachePromise.push(
+          cache.keys(`${key1}*`),
+          cache.keys(`${key2}*`),
+        )
+        logger.info(`Old profileId: ${profile.id}, key1: ${key1}, key2: ${key2}.`)
+      }
+
+      if (cachePromise.length) {
+        try {
+          const keysArray = await Promise.all(cachePromise)
+          if (keysArray.length) {
+            for (const keys of keysArray) {
+              if (keys?.length) {
+                await cache.del(...keys)
+                logger.info(`Key deleted: ${keys}`)
+              }
+            }
+          }
+          cachePromise = []
+        } catch (err) {
+          logger.info(err, 'Error while clearing cache...')
+        }
+      }
+    }
+  } catch (err) {
+    logger.error({ err, existingNFT }, `Error in deleteCacheKeys for existingNFT: ${existingNFT.id}`)
+  }
+}
+
+/**
+ * Handles the NFT ownership change and related caching.
+ * @param wallet - The wallet of the new owner.
+ * @param existingNFT - The existing NFT entity in the database.
+ * @param csNewOwner - The new owner's checksum address.
+ * @param chainId - The chain ID of the blockchain network.
+ * @returns The updated NFT entity.
+ */
+const updateNFTWithWallet = async (
+  wallet: entity.Wallet,
+  existingNFT: entity.NFT,
+  csNewOwner: string,
+  chainId: string,
+): Promise<entity.NFT> => {
+  // Check if the ownership has changed and, if so, remove the edge of the previous profile
+  if (existingNFT.userId !== wallet.userId || existingNFT.walletId !== wallet.id) {
+    await removePreviousProfileEdge(existingNFT.id)
+
+    // Delete cache keys only when the NFT userId or walletId exists
+    if (existingNFT.userId || existingNFT.walletId) {
+      await deleteCacheKeys(existingNFT, chainId)
+    } else {
+      logger.info(`NFT wallet id and user id are null or undefined. WalletId: ${existingNFT.walletId}, UserId: ${existingNFT.userId}`)
+    }
+
+    // If this NFT is a profile NFT, process it as such
+    if (ethers.utils.getAddress(existingNFT.contract) ===
+      ethers.utils.getAddress(contracts.nftProfileAddress(chainId))) {
+      await processProfileNFT(existingNFT)
+    }
+  }
+
+  const updatedNFT = await repositories.nft.updateOneById(existingNFT.id, {
+    owner: csNewOwner,
+    userId: wallet.userId,
+    walletId: wallet.id,
+  })
+
+  if (updatedNFT) {
+    await nftService.indexNFTsOnSearchEngine([updatedNFT])
+  }
+
+  // Handle new owner profile
+  await handleNewOwnerProfile(wallet, updatedNFT, chainId)
+
+  return updatedNFT
+}
+
+/**
+ * Updates an NFT without an associated wallet.
+ * @param existingNFT - The existing NFT entity in the database.
+ * @param csNewOwner - The new owner's checksum address.
+ * @returns The updated NFT entity.
+ */
+const updateNFTWithoutWallet = async (
+  existingNFT: entity.NFT,
+  csNewOwner: string,
+): Promise<entity.NFT> => {
+  const updatedNFT = await repositories.nft.updateOneById(existingNFT.id, {
+    owner: csNewOwner,
+    walletId: null,
+    userId: null,
+    profileId: null,
+  })
+  return updatedNFT
+}
+
+/**
+ * Updates the ownership of an NFT atomically.
+ * @param contract - The contract address of the NFT.
+ * @param tokenId - The token ID of the NFT.
+ * @param prevOwner - The previous owner's address.
+ * @param newOwner - The new owner's address.
+ * @param chainId - The chain ID of the blockchain network.
+ * @returns A Promise that resolves to void.
+ */
 export const atomicOwnershipUpdate = async (
   contract: string,
   tokenId: string,
@@ -27,205 +266,67 @@ export const atomicOwnershipUpdate = async (
   chainId: string,
 ): Promise<void> => {
   const csContract = checksumAddress(contract),
-    csPrevOwner =  checksumAddress(prevOwner),
+    csPrevOwner = checksumAddress(prevOwner),
     csNewOwner = checksumAddress(newOwner)
-  let hexTokenId
+
+  let hexTokenId: string
 
   try {
     hexTokenId = helper.bigNumberToHex(tokenId)
   } catch (err) {
     logger.error(err, 'Error while casting tokenId to hex')
+    return
   }
-  if (csContract && csPrevOwner && csNewOwner && hexTokenId) {
-    logger.info(
-      `NFT ownership transfer started for contract: ${csContract},
+
+  if (!csContract || !csPrevOwner || !csNewOwner || !hexTokenId) {
+    logger.error(
+      `Invalid NFT ownership transfer parameters for
+      contract: ${csContract},
       tokenId: ${hexTokenId},
       previous owner: ${csPrevOwner},
       new owner: ${csNewOwner}`,
     )
-    try {
-      const wallet = await repositories.wallet.findByNetworkChainAddress(
-        'ethereum',
-        chainId,
-        csNewOwner,
-      )
-      let updatedNFT: entity.NFT
-      const existingNFT: entity.NFT = await repositories.nft.findOne({
-        where: {
-          contract: csContract,
-          tokenId: hexTokenId,
-        },
-      })
-      if (existingNFT) {
-        if (!wallet) {
-          updatedNFT = await repositories.nft.updateOneById(existingNFT.id, {
-            owner: csNewOwner,
-            walletId: null,
-            userId: null,
-            profileId: null,
-          })
-        } else {
-          let cachePromise = []
-          // if this NFT is existing and owner changed, we change its ownership...
-          if (existingNFT.userId !== wallet.userId || existingNFT.walletId !== wallet.id) {
-            // we remove edge of previous profile
-            // logger.log(`&&& updateNFTOwnershipAndMetadata: existingNFT.userId ${existingNFT.userId}, userId ${userId}, existingNFT.walletId ${existingNFT.walletId}, walletId ${walletId}`)
+    return
+  }
 
-            await repositories.edge.hardDelete({
-              thatEntityId: existingNFT.id,
-              edgeType: defs.EdgeType.Displays,
-            })
-            
-            // delete cache keys only when nft userId or walletId exist
-            if (existingNFT.userId || existingNFT.walletId) {
-              const oldOwnerProfileQuery = {
-                ownerWalletId: existingNFT.walletId,
-                ownerUserId: existingNFT.userId,
-              }
-             
-              const oldOwnerProfileCount: number = await repositories.profile
-                .count(oldOwnerProfileQuery)
-    
-              logger.log(`Old owner profiles count: ${oldOwnerProfileCount} - query: ${JSON.stringify(oldOwnerProfileQuery)}`)
-    
-              for (let i=0; i < oldOwnerProfileCount; i+= MAX_PROCESS_BATCH_SIZE) {
-                const oldOwnerProfiles: entity.Profile[] = await repositories.profile.find({
-                  where:  oldOwnerProfileQuery,
-                  skip: i,
-                  take: MAX_PROCESS_BATCH_SIZE,
-                  select: {
-                    id: true,
-                    url: true,
-                  },
-                })
-              
-                logger.info(`Old owner profiles length: ${oldOwnerProfiles.length}, batch: ${i}`)
-                for (const profile of oldOwnerProfiles) {
-                  const key1 = `${CacheKeys.PROFILE_SORTED_VISIBLE_NFTS}_${chainId}_${profile.id}`,
-                    key2 = `${CacheKeys.PROFILE_SORTED_NFTS}_${chainId}_${profile.id}`
-                  cachePromise.push(
-                    cache.keys(`${key1}*`),
-                    cache.keys(`${key2}*`),
-                  )
-                  logger.info(`Old profileId: ${profile.id}, key1: ${key1}, key2: ${key2}.`)
-                }
-    
-                if (cachePromise.length) {
-                  try {
-                    const keysArray = await Promise.all(cachePromise)
-                    if (keysArray.length) {
-                      for (const keys of keysArray) {
-                        if (keys?.length) {
-                          await cache.del(...keys)
-                          logger.info(`Key deleted: ${keys}`)
-                        }
-                      }
-                    }
-                    cachePromise = []
-                  } catch (err) {
-                    logger.info(err, 'Error while clearing cache...')
-                  }
-                }
-              }
-            } else {
-              logger.info(`NFT wallet id and user id are null or undefined. WalletId: ${existingNFT.walletId}, UserId: ${existingNFT.userId}`)
-            }
-         
-            // if this NFT is a profile NFT...
-            if (ethers.utils.getAddress(existingNFT.contract) ==
-                      ethers.utils.getAddress(contracts.nftProfileAddress(chainId))) {
-              logger.info(`Profile is being processed as a Profile NFT - contract ${existingNFT.contract}, tokenId: ${hexTokenId}`)
-              const previousWallet = await repositories.wallet.findById(existingNFT.walletId)
-              if (previousWallet) {
-                const profile: entity.Profile = await repositories.profile.findOne({ where: {
-                  tokenId: BigNumber.from(existingNFT.tokenId).toString(),
-                  ownerWalletId: previousWallet.id,
-                  ownerUserId: previousWallet.userId,
-                } })
-                // if this NFT was previous owner's preferred profile...
-                if (profile && profile?.id === previousWallet.profileId) {
-                  await repositories.wallet.updateOneById(previousWallet.id, {
-                    profileId: null,
-                  })
-                }
-              } else {
-                logger.info(`previous wallet for existing NFT ${existingNFT.id} is undefined`)
-              }
-            }
+  try {
+    const wallet = await repositories.wallet.findByNetworkChainAddress(
+      'ethereum',
+      chainId,
+      csNewOwner,
+    )
 
-            updatedNFT = await repositories.nft.updateOneById(existingNFT.id, {
-              owner: csNewOwner,
-              userId: wallet.userId,
-              walletId: wallet.id,
-            })
-            if(updatedNFT) {
-              await nftService.indexNFTsOnSearchEngine([updatedNFT])
-            }
+    const existingNFT: entity.NFT = await repositories.nft.findOne({
+      where: {
+        contract: csContract,
+        tokenId: hexTokenId,
+      },
+    })
 
-            // new owner profile
-            const profileQuery = {
-              ownerWalletId: wallet.id,
-              ownerUserId: wallet.userId,
-            }
-            const newOwnerProfileCount: number = await repositories.profile.count(profileQuery)
-  
-            logger.info(`New owner profiles count: ${newOwnerProfileCount}`)
-  
-            for (let i=0; i < newOwnerProfileCount; i += MAX_PROCESS_BATCH_SIZE) {
-              const newOwnerProfiles: entity.Profile[] = await repositories.profile.find({
-                where:  profileQuery,
-                skip: i,
-                take: MAX_PROCESS_BATCH_SIZE,
-                select: {
-                  id: true,
-                  url: true,
-                },
-              })
-  
-              logger.info(`New owner profiles length: ${newOwnerProfiles.length}, batch: ${i}`)
-  
-              for (const profile of newOwnerProfiles) {
-                //
-                try {
-                  await repositories.edge.save({
-                    thisEntityType: defs.EntityType.Profile,
-                    thatEntityType: defs.EntityType.NFT,
-                    thisEntityId: profile.id,
-                    thatEntityId: updatedNFT.id,
-                    edgeType: defs.EdgeType.Displays,
-                    hide: true,
-                  })
-                  logger.info(`updated edges for profile in ownership for profileId: ${profile.id}, url: ${profile.url}.`)
-                } catch (err) {
-                  logger.error(err, `Error in updateEdgesWeightForProfile in ownership for profileId:${profile.id}, url: ${profile.url}`)
-                }
-
-                try {
-                  await nftService.syncEdgesWithNFTs(profile.id)
-                  logger.info(`synced edges with NFTs for profile in ownership for profileId: ${profile.id}, url: ${profile.url}`)
-                } catch (err) {
-                  logger.error(err, `Error in syncEdgesWithNFTs in ownership for profileId:${profile.id}, url: ${profile.url}`)
-                }
-
-                try {
-                  await Promise.all([
-                    cache.zrem(`${CacheKeys.PROFILES_IN_PROGRESS}_${chainId}`, [profile.url]),
-                    cache.zrem(`${CacheKeys.UPDATED_NFTS_PROFILE}_${chainId}`, [profile.url]),
-                  ])
-                } catch (err) {
-                  logger.error(err, `Error in clearing cache for new profileId: ${profile.id}, url: ${profile.url}`)
-                }
-              }
-            }
-          }
-        }
+    if (existingNFT) {
+      if (!wallet) {
+        const updatedNFT: entity.NFT = await updateNFTWithoutWallet(
+          existingNFT,
+          csNewOwner,
+        )
+        logger.info(`Ownership transfer for non-user-owned NFT ${updatedNFT.id} completed.`)
+      } else {
+        const updatedNFT: entity.NFT = await updateNFTWithWallet(
+          wallet,
+          existingNFT,
+          csNewOwner,
+          chainId,
+        )
+        logger.info(`Ownership transfer for user-owned NFT ${updatedNFT.id} completed.`)
       }
-    } catch (err) {
-      logger.error(err, `Ownership transfer error for
-        contract: ${csContract},
-        tokenId: ${hexTokenId},
-        previous owner: ${csPrevOwner},
-        new owner: ${csNewOwner}`)
+    } else {
+      // TODO: hande new NFT
     }
+  } catch (err) {
+    logger.error(err, `Ownership transfer error for
+      contract: ${csContract},
+      tokenId: ${hexTokenId},
+      previous owner: ${csPrevOwner},
+      new owner: ${csNewOwner}`)
   }
 }
---
+++ packages/stream/src/service/streamingfast.ts
@@ -0,0 +1,133 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { BigNumber } from 'ethers'
+import { Client } from 'pg'
+import { EventEmitter } from 'stream'
+
+import { _logger, db, helper } from '@nftcom/shared'
+
+import { cache, CacheKeys } from '../service/cache'
+import { getLatestBlockNumber } from '../utils'
+import { atomicOwnershipUpdate } from './ownership'
+
+const repositories = db.newRepositories()
+const connectionString = 'postgresql://app:nftcom1234@sf-substreams-instance-1.clmsk3iud7e0.us-east-1.rds.amazonaws.com:5432/app'
+const logger = _logger.Factory('STREAMINGFAST')
+const client = new Client({ connectionString })
+const nftDoesNotExist = new EventEmitter()
+const blockRange = 200                    // 200 blocks padding for internal of latest block numbers
+const REMOVE_SPAM_FILTER = true           // filter out spam transfers
+const ONLY_OFFICIAL_FILTER = false        // only listen to official contracts
+const ONLY_EXISTING_NFT_FILTER = true     // only listen to existing NFTs
+
+let latestBlockNumber: number = null
+let interval: NodeJS.Timeout = null
+
+const handleFilter = async (contractAddress: string, tokenId: string): Promise<boolean> => {
+  if (ONLY_OFFICIAL_FILTER) {
+    const collection = await repositories.collection.findOne({
+      where: { contract: helper.checkSum(contractAddress) },
+    })
+    if (!collection?.isOfficial) return false
+  }
+
+  if (REMOVE_SPAM_FILTER) {
+    if (await cache.sismember(
+      CacheKeys.SPAM_COLLECTIONS, helper.checkSum(contractAddress),
+    )) return false
+  }
+
+  if (ONLY_EXISTING_NFT_FILTER) {
+    const nftExists = await repositories.nft.exists({
+      contract: helper.checkSum(contractAddress),
+      tokenId,
+    })
+    if (!nftExists) {
+      nftDoesNotExist.emit('nft', { contractAddress, tokenId })
+      return nftExists
+    }
+  }
+
+  return true
+}
+
+nftDoesNotExist.on('nft', ({ contractAddress, tokenId }) => {
+  logger.warn({ contractAddress, tokenId }, 'NFT does not exist')
+})
+
+const ensureHexPrefix = (value: string): string => {
+  return value.startsWith('0x') ? value : `0x${value}`
+}
+
+const handleNotification = async (msg: any): Promise<void> => {
+  const start = new Date().getTime()
+  // if latestBlockNumber is not set, call getLatestBlockNumber and store the result in latestBlockNumber
+  if (latestBlockNumber === null) {
+    latestBlockNumber = await getLatestBlockNumber()
+    logger.info(`[handleNotification] - the latest block number is ${latestBlockNumber}`)
+  }
+
+  const [schema, blockNumber, tokenId, contractAddress, quantity, fromAddress, toAddress, txHash, timestamp] = msg.payload.split('|')
+  const blockDifference = Math.abs(latestBlockNumber - Number(blockNumber))
+  const hexTokenId = ensureHexPrefix(tokenId)
+  const hexContractAddress = ensureHexPrefix(contractAddress)
+  const hexFromAddress = ensureHexPrefix(fromAddress)
+  const hexToAddress = ensureHexPrefix(toAddress)
+  const hexTxHash = ensureHexPrefix(txHash)
+
+  if (blockDifference <= blockRange &&
+    await handleFilter(contractAddress, hexTokenId)
+  ) {
+    if (fromAddress === '0x0000000000000000000000000000000000000000') {
+      logger.info(`streamingFast: [MINTED]: ${schema}/${hexContractAddress}/${hexTokenId} to ${hexToAddress}, ${Number(quantity) > 1 ? `quantity=${quantity}, ` : ''}https://etherscan.io/tx/${hexTxHash}`)
+    } else if (hexToAddress === '0x0000000000000000000000000000000000000000') {
+      logger.info(`streamingFast: [BURNED]: ${schema}/${hexContractAddress}/${hexTokenId} from ${hexFromAddress}, ${Number(quantity) > 1 ? `quantity=${quantity}, ` : ''}https://etherscan.io/tx/${hexTxHash}`)
+    } else {
+      const start2 = new Date().getTime()
+      await atomicOwnershipUpdate(
+        hexContractAddress,
+        hexTokenId,
+        hexFromAddress,
+        hexToAddress,
+        '1', // mainnet ETH
+      )
+      logger.info(`streamingFast (preChecks took ${new Date().getTime() - start}ms, atomicOwnershipUpdate took ${new Date().getTime() - start2} ms): [TRANSFERRED]: ${schema}/${hexContractAddress}/${hexTokenId} from ${hexFromAddress} to ${hexToAddress}, ${Number(quantity) > 1 ? `quantity=${quantity}, ` : ''}https://etherscan.io/tx/${hexTxHash}`)
+    }
+  } else {
+    logger.warn({ schema, blockNumber, hexTokenId, hexContractAddress, quantity, hexFromAddress, hexToAddress, txHash, timestamp }, 'Filtered Transfer')
+  }
+}
+
+export function startStreamingFast(): void {
+  logger.info('---------> ⚡️ starting streaming fast listener, waiting for new transfers...')
+  if (interval || client.listeners('notification').length) {
+    logger.warn('StreamingFast is already running')
+    return
+  }
+
+  client.connect()
+
+  client.query('LISTEN transfers')
+
+  // Call getLatestBlockNumber every 5 minutes and store the result in latestBlockNumber
+  interval = setInterval(async () => {
+    latestBlockNumber = await getLatestBlockNumber()
+  }, 1 * 60 * 1000)
+
+  logger.info(`the latest block number is ${latestBlockNumber}`)
+
+  client.on('notification', handleNotification)
+}
+
+export function stopStreamingFast(): void {
+  logger.info('---------> 🛑 stopping streaming fast listener')
+  if (!interval || !client.listeners('notification').length) {
+    logger.warn('StreamingFast is not running')
+    return
+  }
+
+  clearInterval(interval)
+  interval = null
+
+  client.off('notification', handleNotification)
+  client.end()
+}
