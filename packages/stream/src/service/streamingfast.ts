a487b4ea16affde4f402c2b76c340b5927363eba
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-03-31 16:34:13 -0400

    fix: fix hex

--- packages/stream/src/service/streamingfast.ts
+++ packages/stream/src/service/streamingfast.ts
@@ -84,8 +84,8 @@ const handleNotification = async (msg: any): Promise<void> => {
         hexFromAddress,
         hexToAddress,
         '1', // mainnet ETH
-        )
-        logger.info(`streamingFast (took ${new Date().getTime() - start2}ms): [MINTED]: ${schema}/${hexContractAddress}/${hexTokenId} to ${hexToAddress}, ${Number(quantity) > 1 ? `quantity=${quantity}, ` : ''}https://etherscan.io/tx/${hexTxHash}`)
+      )
+      logger.info(`streamingFast (took ${new Date().getTime() - start2}ms): [MINTED]: ${schema}/${hexContractAddress}/${hexTokenId} to ${hexToAddress}, ${Number(quantity) > 1 ? `quantity=${quantity}, ` : ''}https://etherscan.io/tx/${hexTxHash}`)
     } else if (hexToAddress === '0x0000000000000000000000000000000000000000') {
       logger.info(`streamingFast: [BURNED]: ${schema}/${hexContractAddress}/${hexTokenId} from ${hexFromAddress}, ${Number(quantity) > 1 ? `quantity=${quantity}, ` : ''}https://etherscan.io/tx/${hexTxHash}`)
     } else {
